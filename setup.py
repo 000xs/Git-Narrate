63246280ac51a0202e344c78bb89c2efdef0d15f
Sithum Sathsara <89584440+SithumSathsaras@users.noreply.github.com>
2025-08-06 13:14:05 -0400

    opein api key inject in build code

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name="git-narrate",
-    version="0.1.1",
+    version="0.1.2",
     description="The Repository Storyteller - Analyze git repos and generate development narratives",
     long_description=long_description,
     long_description_content_type="text/markdown",
